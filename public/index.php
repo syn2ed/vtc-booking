34676ac45a02c3f03505bef3a57fce6c4a7441f6
Ayoub LAAROBI <alaarobi@prismamedia.com>
2019-12-01 15:06:45 +0100

    fix bug sf

--- public/index.php
+++ public/index.php
@@ -22,11 +22,16 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+
 Request::setTrustedProxies(
-// trust *all* requests
-    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
-    Request::HEADER_X_FORWARDED_ALL
+// trust *all* requests (the 'REMOTE_ADDR' string is replaced at
+// run time by $_SERVER['REMOTE_ADDR'])
+    ['127.0.0.1', 'REMOTE_ADDR'],
+
+    // if you're using ELB, otherwise use a constant from above
+    Request::HEADER_X_FORWARDED_AWS_ELB
 );
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
